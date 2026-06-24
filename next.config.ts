b86f7ca44aae6ab56b8c210d728348838caeda5b
Stefano Naranjo <stefanosckost@Stefanos-MacBook-Air.local>
2026-06-24 13:23:30 -0400

    Redirect www domain to apex

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,21 @@ const nextConfig: NextConfig = {
     "192.168.1.194",
     "localhost",
   ],
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.snvisuals.studio",
+          },
+        ],
+        destination: "https://snvisuals.studio/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
